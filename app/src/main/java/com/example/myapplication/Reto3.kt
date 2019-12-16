1408c3e9bdef47ee76cdc7cde6d70635df26b7ab
Hector <hposecarames@danielcastelao.org>
2019-12-16 21:52:23 +0100

    Implementado reto 3

--- app/src/main/java/com/example/myapplication/Reto3.kt
+++ app/src/main/java/com/example/myapplication/Reto3.kt
@@ -1,7 +1,26 @@
 package com.example.myapplication
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.reto3.*
+import org.jetbrains.anko.db.INTEGER
 
-class Reto3 : AppCompatActivity(){
 
+class Reto3 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.reto3)
+
+        var nu1 = 0
+        var nu2 = 0
+
+        btnmultiplicar.setOnClickListener {
+
+            nu1 = Integer.parseInt(num1.text.toString())
+            nu2 = Integer.parseInt(num2.text.toString())
+
+            resultado.text = (nu1 * nu2).toString()
+        }
+    }
 }
